620744fdc0a974ddaac9de4f586af956817c91b1
Joseph Nam <josephknam@gmail.com>
2017-04-25 22:31:01 -0400

    long time

--- models/models.go
+++ models/models.go
@@ -9,7 +9,8 @@ import (
 // Username is a string representing the username they use to log in
 // Password is a string representing the password they use to log in TODO: bcrypt
 type User struct {
-	ID       bson.ObjectId `bson:"_id,omitempty"`
-	Username string
-	Password string
+	ID         bson.ObjectId `bson:"_id,omitempty"`
+	Username   string
+	Password   string
+	CustomerID string `bson:", omitempty"`
 }
--- server.go
+++ server.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-
+	"fmt"
 	"net/http"
 
 	"gopkg.in/mgo.v2"
@@ -10,7 +10,11 @@ import (
 
 	"github.com/JosephNam/freedom-server/dao"
 	"github.com/JosephNam/freedom-server/models"
+
 	"github.com/julienschmidt/httprouter"
+
+	"github.com/stripe/stripe-go"
+	//	"github.com/stripe/stripe-go/charge"
 )
 
 var session *mgo.Session
@@ -36,6 +40,20 @@ func bsonify(r *http.Request) bson.M {
 	body := bson.M{}
 	check(json.NewDecoder(r.Body).Decode(&body))
 	return body
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	c := session.DB("freedom").C("user")
+	request := bsonify(r)
+	doc := models.User{}
+	writeSuccess := dao.Create(c, request)
+	if writeSuccess {
+		createResponse(request, &w)
+		return
+	}
+	createFailureResponse("Something went wrong connecting your card", &w)
+	return
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -69,6 +87,54 @@ func handleRegister(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 	return
 }
 
+// FreedomCharge ...
+/*
+type FreedomCharge struct {
+	Amount   float64
+	Name     string
+	ExpMonth string
+	ExpYear  string
+	Number   string
+	CVC      string
+}
+*/
+
+func handleCharge(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	// c := session.DB("freedom").C("tithe")
+	request := bsonify(r)
+	fmt.Println(request)
+	createResponse(bson.M{"ch_id": "testing"}, &w)
+	return
+	/*
+		c := FreedomCharge{
+			Amount:   request["amount"].(float64),
+			Name:     request["name"].(string),
+			ExpMonth: request["expMonth"].(string),
+			ExpYear:  request["expYear"].(string),
+			Number:   request["number"].(string),
+			CVC:      request["cvc"].(string),
+		}
+		chargeParams := &stripe.ChargeParams{
+			Amount:   uint64(c.Amount),
+			Currency: "usd",
+			Desc:     "Charge for " + c.Name,
+		}
+		chargeParams.SetSource(&stripe.CardParams{
+			Name:   c.Name,
+			Number: c.Number,
+			Month:  c.ExpMonth,
+			Year:   c.ExpYear,
+			CVC:    c.CVC,
+		})
+		chargeParams.AddMeta("key", "value")
+
+		charge.New(chargeParams)
+			check(err)
+			fmt.Println(ch.ID)
+	*/
+}
+
 func main() {
 	var connErr error
 	session, connErr = mgo.Dial("mongodb://dev:dev@ds061345.mlab.com:61345/freedom")
@@ -78,8 +144,12 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	router := httprouter.New()
+
+	stripe.Key = "sk_test_xW56HPAT1oXJ1YHC9tXIE8sB"
+
 	router.POST("/api/authenticate", handleLogin)
 	router.POST("/api/register", handleRegister)
+	router.POST("/api/charge", handleCharge)
 
 	http.ListenAndServe(":5000", router)
 }
